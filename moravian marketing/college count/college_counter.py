a92cfaab2e1f2fbed574bde65cdb995e99f1f3ad
naveenailawadi <48597625+naveenailawadi@users.noreply.github.com>
2019-06-03 10:57:51 -0400

    Update college_counter.py

--- moravian marketing/college count/college_counter.py
+++ moravian marketing/college count/college_counter.py
@@ -1,3 +1,12 @@
+'''
+I had to count the colleges that the seniors at Moravian
+Academy will be attending in the fall. In addition, 
+I had to sort them alphabetically. All of this would be done 
+from a csv file. Instead of doing them by hand, here is a 
+program that does the same thing and exports it as a word document.
+'''
+
+
 import pandas
 from collections import Counter
 from docx import Document
